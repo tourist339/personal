7c084b0dba2a67222829d92bcefe8e7ffa8d2a69
tourist339 <kartikkapoor3390@gmail.com>
2021-11-15 19:39:04 -0600

    upgraded routing for gh pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/personal/">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
